766364cc48897a79a5219012611120dc34b9bb10
midhun joseph <midhunjoseph.joseph000@gmail.com>
2023-08-06 14:58:16 +0530

    add registration component new ui

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/videoAppServices/login.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({})
+  @Input() emailValue: string | undefined;
+  @Output() emailValueChange: EventEmitter<string> = new EventEmitter;
+
+  constructor(private formBuilder: FormBuilder,
+    public loginService: LoginService){}
+
+  ngOnInit(): void {
+      this.loginService.isNewUser = true;
+      this.registerForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        useremail: ['',Validators.required],
+        password: ['', Validators.required]
+      })
+
+      if(this.emailValue !== null){
+        this.registerForm.patchValue({
+          useremail: this.emailValue
+        })
+      }
+  }
+
+  public onSubmit(){
+    if(this.registerForm.valid){
+      const formData = this.registerForm.value;
+      console.log("register",formData);
+      
+    }
+  }
+
+  public onClickLogin(){
+    this.loginService.isNewUser= false;
+    this.emailValueChange.emit(this.registerForm.value.useremail)
+    console.log("gare");
+    
+  }
 
 }
