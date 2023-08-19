91e05630f891098f1c452e1d200ffcfae91aee68
midhun joseph <midhunjoseph.joseph000@gmail.com>
2023-08-19 18:17:13 +0530

    add video streaming UI

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { VcConstants } from 'src/app/utils/vc-Constants';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
 
+  constructor(private router: Router,
+    private vcConstants: VcConstants){}
+
+  ngOnInit(): void {
+      
+  }
+
+  public newMeeting(){
+    this.router.navigateByUrl(this.vcConstants.web_router_link.video)
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { VcConstants } from 'src/app/utils/vc-Constants';
 import { LoginService } from 'src/app/videoAppServices/login.service';
 
@@ -12,31 +13,38 @@ import { LoginService } from 'src/app/videoAppServices/login.service';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({})
 
-  constructor(private formbuilder:FormBuilder,
+  constructor(private formbuilder: FormBuilder,
     public loginservice: LoginService,
     private snackbar: MatSnackBar,
-    private vcConstants: VcConstants){}
+    private vcConstants: VcConstants,
+    private zone: NgZone,
+    private router: Router) { }
 
   ngOnInit(): void {
-    this.loginservice.isNewUser= false;
-      this.loginForm = this.formbuilder.group({
-        username: ['', Validators.required],
-        password: ['', Validators.required]
-      })
+    this.loginservice.isNewUser = false;
+    this.loginForm = this.formbuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
-  public  onSubmit() {
+  public onSubmit() {
     if (this.loginForm.valid) {
       const formData = this.loginForm.value;
       // Here you can handle the login logic, e.g., authentication.
       console.log('Login form submitted:', formData);
       this.loginservice.login(formData).subscribe(
-        result =>{
+        result => {
           console.log(result);
-          
+          if (result) {
+            setTimeout(() => {
+              this.onSuccess();
+            }, 1000);
+          }
+
         },
-        (err)=>{
-          this.snackbar.open(this.vcConstants.dialogMessage.invalidPassword,'',{
+        (err) => {
+          this.snackbar.open(this.vcConstants.dialogMessage.invalidPassword, '', {
             duration: 3000,
             panelClass: this.vcConstants.snackbarType.error
           })
@@ -45,16 +53,22 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  public newUserAccount(){
-    this.loginservice.isNewUser= true;
+  public newUserAccount() {
+    this.loginservice.isNewUser = true;
   }
 
-  public onEmailChanged(emailChanged:string){
-    if(emailChanged !== null){
+  public onEmailChanged(emailChanged: string) {
+    if (emailChanged !== null) {
       this.loginForm.patchValue({
         username: emailChanged,
       })
     }
   }
 
+  private onSuccess() {
+    this.zone.run(() => {
+      this.router.navigateByUrl(this.vcConstants.web_router_link.home);
+    });
+  }
 }
+
